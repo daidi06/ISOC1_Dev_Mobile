21f1d4cbe9f3660a6ba48d4ef43f2d0e6c2816c6
Didier Ilboudo <ildidier@hotmail.com>
2023-07-27 20:35:21 +0000

    Validate all changes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'form',
+    pathMatch: 'full'
+  },
+  {
+    path: 'form',
+    loadChildren: () => import('./pages/form/form.module').then( m => m.FormPageModule)
+  },
+  {
+    path: 'picture',
+    loadChildren: () => import('./pages/picture/picture.module').then( m => m.PicturePageModule)
+  },
+  {
+    path: 'bible',
+    loadChildren: () => import('./pages/bible/bible.module').then( m => m.BiblePageModule)
+  },
+  {
+    path: 'identification',
+    loadChildren: () => import('./pages/identification/identification.module').then( m => m.IdentificationPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/pages/picture/picture.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Photo } from '@capacitor/camera';
+
+import { PhotoService } from 'src/app/services/photo.service';
+
+@Component({
+  selector: 'app-picture',
+  templateUrl: './picture.page.html',
+  styleUrls: ['./picture.page.scss'],
+})
+export class PicturePage implements OnInit {
+  public photo: Photo | undefined = undefined;
+
+  constructor(public photoService : PhotoService) { }
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  // public capture() {
+  //  this.photoService.addPhotoToGallery().then((_photo: Photo) => {
+  //   this.photo = _photo;
+  //}); 
+    
+  //}
+
+}
